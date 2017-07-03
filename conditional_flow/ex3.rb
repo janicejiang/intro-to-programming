9e1c90cbd4c9cf89d6444e66213f7ba6f9a83a32
janice <janicejiang.d@gmail.com>
2017-07-03 14:25:33 +0800

    implement conditional flow exercises

---
+++ conditional_flow/ex3.rb
@@ -0,0 +1,15 @@
+def evaluate_number(number)
+  if number < 0
+    puts "You can't enter a negative number!"
+  elsif number <= 50
+    puts "#{number} is between 0 and 50"
+  elsif number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is above 100"
+  end
+end
+
+puts "Please enter a number between 0 and 100."
+number = gets.chomp.to_i
+evaluate_number(number)
---
+++ conditional_flow/ex6.rb
@@ -0,0 +1,3 @@
+# You get this error because the end in the code above gets matched with the
+# if..else statement. The error message is telling us that the interpreter was
+# expecting the keyword end to close off our equal_to_four method, that end was not found.
