01fe19b5154267c6892526c5e2f0189bf8eccb06
janice <janicejiang.d@gmail.com>
2017-07-03 15:11:29 +0800

    implement arrays exercises

---
+++ arrays/ex4.rb
@@ -0,0 +1,5 @@
+# 3
+
+# NoMethodError: undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index>
+
+# 8
---
+++ arrays/ex6.rb
@@ -0,0 +1,2 @@
+# You are attempting to set the value of an item in an array using a string as the key. Arrays are indexed with integers, not strings. You would modify the array by doing the following:
+# names[3] = 'jody'
